f4c5a6efec8c2dc5a94ea88b49b20744a9c50e59
Anuchit Chalothorn <anoochit@gmail.com>
2023-08-01 09:14:05 +0700

    update post gen for awesome dashboard

--- bricks/awesome_dashboard/hooks/post_gen.dart
+++ bricks/awesome_dashboard/hooks/post_gen.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+Future<void> run(HookContext context) async {
+  final progress = context.logger.progress('Installing packages');
+
+  // Run `flutter packages get` after generation.
+  await Process.run('flutter', ['packages', 'get']);
+
+  progress.complete();
 }
